0b5aa4a8222ab41603fc0ded75fe7dec87e2ea36
Caroline <carolinesalibc@gmail.com>
2021-12-14 08:21:24 -0700

    Add merge sort to performance algorithm

--- sort/performance.rb
+++ sort/performance.rb
@@ -5,8 +5,10 @@ require "benchmark"
 numbers = (1...16_884).to_a.shuffle
 numbers_for_selection_sort = numbers.clone
 numbers_for_insertion_sort = numbers.clone
+numbers_for_merge_sort = numbers.clone
 
 Benchmark.bm do |x|
   x.report { selection_sort(numbers_for_selection_sort) }
   x.report { insertion_sort(numbers_for_insertion_sort) }
+  x.report { insertion_sort(numbers_for_merge_sort) }
 end
