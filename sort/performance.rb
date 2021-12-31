c4059ed21d84d1646aa88bbfed6acce7271b2c51
Caroline Canto <carolinecanto@Carolines-MacBook-Pro.local>
2021-12-31 09:46:48 -0700

    Fix quick sort algorithm performance

--- sort/performance.rb
+++ sort/performance.rb
@@ -8,10 +8,12 @@ numbers_for_selection_sort = numbers.clone
 numbers_for_insertion_sort = numbers.clone
 numbers_for_merge_sort = numbers.clone
 numbers_for_quick_sort = numbers.clone
+ruby_sort = numbers.clone
 
 Benchmark.bm do |x|
   x.report { selection_sort(numbers_for_selection_sort) }
   x.report { insertion_sort(numbers_for_insertion_sort) }
   x.report { insertion_sort(numbers_for_merge_sort) }
   x.report { quick_sort(numbers_for_quick_sort) }
+  x.report { ruby_sort.sort }
 end
--- sort/quick_sort.rb
+++ sort/quick_sort.rb
@@ -10,31 +10,31 @@ def switch(array, index1, index2)
   array
 end
 
-def part(array, pivot)
-  smaller_numbers = 0
+def part(array, start_index, end_index)
+  pivot_index = end_index
+  smaller_numbers = start_index
 
-  (0...pivot).each do |i|
-    if array[i] < array[pivot]
+  (start_index...end_index).each do |i|
+    if array[i] <= array[pivot_index]
       switch(array, i, smaller_numbers)
       smaller_numbers += 1
     end
   end
 
-  switch(array, pivot, smaller_numbers)
+  switch(array, pivot_index, smaller_numbers)
   smaller_numbers
 end
 
 def sort(array, start_index, end_index)
-  elements = end_index - start_index
-  if elements > 1
-    new_pivot = part(array, end_index - 1)
-    sort(array, start_index, new_pivot)
-    sort(array, new_pivot + 1, end_index)
-  end
+  return unless start_index < end_index
+
+  new_pivot = part(array, start_index, end_index)
+  sort(array, start_index, new_pivot - 1)
+  sort(array, new_pivot + 1, end_index)
 end
 
 def quick_sort(array)
-  sort(array, 0, array.length)
+  sort(array, 0, array.length - 1)
 end
 
 quick_sort(array)
